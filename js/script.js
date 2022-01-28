46e02fe2bcf5f5af28d7911e4abad94144d2c788
Aleks <ogarsanya@gmail.com>
2022-01-28 13:11:30 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 8;
+// console.log(number);
+// const obj = {
+//     a: 50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+// alert('Hello');
+
+// const result = confirm("are you here?");
+// console.log(result);
+
+// const answ = prompt("Вам есть 18?", "18");
+// console.log(answ);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?');
+// answers[1] = prompt('Как ваша фамилия?');
+// answers[2] = prompt('Сколько вам лет?');
+
+// console.log(typeof(answers));
+
+// const category = 'toys';
+// console.log(`https:/someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+
+// alert(`Привет, ${user}`);
+
+// console.log(4 + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+//     incr++;
+
+const isChecked = true,
+        isClose = false;
+
+console.log(isChecked || isClose);
